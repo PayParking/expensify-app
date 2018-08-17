1957dc1b0d338deabc8e19ae0b1da5080feaa6d6
Jun Kim <junkim0507@gmail.com>
2018-08-17 12:52:28 -0700

    Setup test database env

---
+++ src/playground/promises.js
@@ -0,0 +1,27 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            name: 'Jun',
+            age: 30
+        });
+        // reject('something went wrong');
+    }, 5000);
+});
+
+console.log('before');
+
+promise.then((data) => {
+    console.log('1', data);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('this is my other promise');
+
+        }, 5000);
+    });
+}).then((str) => {
+    console.log('does this run?', str);
+}).catch((error) => {
+    console.log('error:', error);
+});
+
+console.log('after');
